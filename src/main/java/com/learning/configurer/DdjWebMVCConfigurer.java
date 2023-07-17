25c88b2245c5860e39568aff1198ad2cb1415421
Everton <everton.nnrb@gmail.com>
2023-07-17 12:05:01 -0400

    add webMvcConfigurer pageable

---
+++ src/main/java/com/learning/configurer/DdjWebMVCConfigurer.java
@@ -0,0 +1,20 @@
+package com.learning.configurer;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class DdjWebMVCConfigurer implements WebMvcConfigurer {
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        //WebMvcConfigurer.super.addArgumentResolvers(resolvers);
+        PageableHandlerMethodArgumentResolver pageable = new PageableHandlerMethodArgumentResolver();
+        pageable.setFallbackPageable(PageRequest.of(0, 3));
+        resolvers.add(pageable);
+    }
+}
